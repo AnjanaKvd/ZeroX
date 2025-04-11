411345caf837e48d0f5dfa595bf06183aaed481c
Ishan Rangajeewa <119867116+ishan-rangajeewa@users.noreply.github.com>
2025-04-12 00:39:21 +0530

    Search product is restored

--- backend/csm/src/main/java/com/zerox/csm/config/SecurityConfig.java
+++ backend/csm/src/main/java/com/zerox/csm/config/SecurityConfig.java
@@ -40,7 +40,8 @@ public class SecurityConfig {
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
                                 "/error",
-                                "/api/images/**"
+                                "/api/images/**",
+                                "/api/productssearch/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
